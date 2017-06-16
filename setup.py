3d0d1a92b37da350cca8d3d63492f2be4eba3d89
David Brown <dmlb2000@gmail.com>
2017-06-15 20:08:45 -0700

    add requirements parsing to setup.py (#14)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 """Setup and install the UniqueID service."""
+from pip.req import parse_requirements
 from setuptools import setup
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
 
 setup(name='PacificaUniqueID',
       version='1.0',
@@ -8,4 +12,5 @@ setup(name='PacificaUniqueID',
       author='David Brown',
       author_email='david.brown@pnnl.gov',
       packages=['uniqueid'],
-      scripts=['UniqueIDServer.py', 'DatabaseCreate.py'])
+      scripts=['UniqueIDServer.py', 'DatabaseCreate.py'],
+      install_requires=[str(ir.req) for ir in INSTALL_REQS])
