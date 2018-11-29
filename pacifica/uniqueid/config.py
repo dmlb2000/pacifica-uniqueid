8e92c3addb86f4d03144fb53f6942ac6997fd093
David Brown <dmlb2000@gmail.com>
2018-11-28 19:14:35 -0800

    add getenv call to config for peewee url (#30)
    This adds a PEEWEE_URL environment variable to the config as
    well as updating the pre-commit config
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- pacifica/uniqueid/config.py
+++ pacifica/uniqueid/config.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Configuration reading and validation module."""
+from os import getenv
 try:
     from ConfigParser import SafeConfigParser
 except ImportError:  # pragma: no cover python 2 vs 3 issue
-    from configparser import SafeConfigParser
+    from configparser import ConfigParser as SafeConfigParser
 from .globals import CONFIG_FILE
 
 
 def get_config():
     """Return the ConfigParser object with defaults set."""
-    configparser = SafeConfigParser({
-        'peewee_url': 'sqliteext:///db.sqlite3'
-    })
+    configparser = SafeConfigParser()
     configparser.add_section('database')
+    configparser.set('database', 'peewee_url', getenv(
+        'PEEWEE_URL', 'sqliteext:///db.sqlite3'))
     configparser.read(CONFIG_FILE)
     return configparser
