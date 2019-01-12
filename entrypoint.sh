375d356ecc1c1d7eb370916518a42efa68a9a195
David Brown <dmlb2000@gmail.com>
2019-01-11 22:21:07 -0800

    fix docker image for newer config (#34)
    This updates the entrypoint and docker image and docker-compose
    for new configurations
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-python DatabaseCreate.py
+if [[ -z $PEEWEE_DATABASE_URL ]] ; then
+  if [[ $PEEWEE_USER && $PEEWEE_PASS ]]; then
+    PEEWEE_USER_PART="${PEEWEE_USER}:${PEEWEE_PASS}@"
+  fi
+  if [[ $PEEWEE_PORT ]] ; then
+    PEEWEE_ADDR_PART="${PEEWEE_ADDR}:${PEEWEE_PORT}"
+  else
+    PEEWEE_ADDR_PART=$PEEWEE_ADDR
+  fi
+  PEEWEE_DATABASE_URL="${PEEWEE_PROTO}://${PEEWEE_USER_PART}${PEEWEE_ADDR_PART}/${PEEWEE_DATABASE}"
+fi
+mkdir ~/.pacifica-uniqueid/
+printf '[database]\npeewee_url = '${PEEWEE_DATABASE_URL}'\n' > ~/.pacifica-uniqueid/config.ini
+python -c 'from pacifica.uniqueid.__main__ import cmd; cmd()' dbsync
 uwsgi \
   --http-socket 0.0.0.0:8051 \
   --master \
   --die-on-term \
   -p 1 \
-  --wsgi-file /usr/src/app/UniqueIDServer.py "$@"
+  --wsgi-file /usr/src/app/pacifica/uniqueid/wsgi.py "$@"
